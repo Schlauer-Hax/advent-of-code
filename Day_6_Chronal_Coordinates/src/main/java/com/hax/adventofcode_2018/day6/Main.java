9a19f1fe017e917bee4edfd452e99db630ee031d
Hax <arzberger52@gmail.com>
2018-12-07 14:06:46 +0100

    Add files via upload

---
+++ Day_6_Chronal_Coordinates/src/main/java/com/hax/adventofcode_2018/day6/Main.java
@@ -0,0 +1,12 @@
+package com.hax.adventofcode_2018.day6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    public void run() {
+
+    }
+}
