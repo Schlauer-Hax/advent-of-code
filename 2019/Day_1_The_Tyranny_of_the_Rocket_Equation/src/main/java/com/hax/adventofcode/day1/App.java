2eeecb1b60a6633316fdffc5bce5003bee8ff52c
Hax <arzberger52@gmail.com>
2019-12-01 17:43:06 +0100

    Added second Part

--- 2019/Day_1_The_Tyranny_of_the_Rocket_Equation/src/main/java/com/hax/adventofcode/day1/App.java
+++ 2019/Day_1_The_Tyranny_of_the_Rocket_Equation/src/main/java/com/hax/adventofcode/day1/App.java
@@ -2,6 +2,7 @@ package com.hax.adventofcode.day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
@@ -14,12 +15,37 @@ public class App {
             System.exit(0);
         }
         try {
-            int sum = 0;
+            ArrayList<Integer> masses = new ArrayList<>();
             Scanner scanner = new Scanner(file);
             while (scanner.hasNext()) {
-                sum = sum + Integer.parseInt(String.valueOf(scanner.nextInt()/3).split(",")[0])-2;
+                masses.add(scanner.nextInt());
+            }
+
+            // First Part
+            int sum = 0;
+            for (Integer mass: masses) {
+                sum = sum + Integer.parseInt(String.valueOf(mass/3).split(",")[0])-2;
             }
             System.out.println(sum);
+
+            // Second Half
+            int sum2 = 0;
+            for (Integer mass: masses) {
+                boolean calculatedfuelfrommass = false;
+                int neededfuel = 0;
+                while (true) {
+                    if (!calculatedfuelfrommass) {
+                        neededfuel = Integer.parseInt(String.valueOf(mass / 3).split(",")[0]) - 2;
+                        calculatedfuelfrommass = true;
+                    } else neededfuel = Integer.parseInt(String.valueOf(neededfuel / 3).split(",")[0]) - 2;
+                    if (neededfuel<=0) {
+                        break;
+                    }
+                    sum2 = sum2 + neededfuel;
+                }
+            }
+
+            System.out.println(sum2);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
