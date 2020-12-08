233acb7fb9aa6f310cadebb0e819223d0ce77dc4
Schlauer-Hax <arzberger52@gmail.com>
2020-12-08 15:05:13 +0100

    Solution for Dec 8th

---
+++ src/main/java/com/hax/adventofcode/solutions/S20/S2008.java
@@ -0,0 +1,75 @@
+package com.hax.adventofcode.solutions.S20;
+
+import com.hax.adventofcode.core.Solution;
+
+import java.util.ArrayList;
+
+public class S2008 implements Solution {
+
+    @Override
+    public void firstPart(String data, String[] lines) {
+        int accumulator = 0;
+        ArrayList<Integer> executed = new ArrayList<>();
+        for (int i = 0; i < lines.length; i++) {
+            String[] split = lines[i].split(" ");
+            if (!executed.contains(i)) {
+                executed.add(i);
+                switch (split[0]) {
+                    case "acc":
+                        accumulator += Integer.parseInt(split[1]);
+                        break;
+
+                    case "jmp":
+                        i += Integer.parseInt(split[1]) - 1;
+                        break;
+
+                    case "nop":
+
+                        break;
+                }
+            } else {
+                System.out.println(accumulator);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void secondPart(String data, String[] lines) {
+        for (int j = 0; j < lines.length; j++) {
+            String[] changedlines = lines.clone();
+
+            if (changedlines[j].startsWith("nop")) changedlines[j] = changedlines[j].replace("nop", "jmp");
+            else if (changedlines[j].startsWith("jmp")) changedlines[j] = changedlines[j].replace("jmp", "nop");
+
+            int accumulator = 0;
+            boolean looped = false;
+            ArrayList<Integer> executed = new ArrayList<>();
+            for (int i = 0; i < changedlines.length; i++) {
+                String[] split = changedlines[i].split(" ");
+                if (!executed.contains(i)) {
+                    executed.add(i);
+                    switch (split[0]) {
+                        case "acc":
+                            accumulator += Integer.parseInt(split[1]);
+                            break;
+
+                        case "jmp":
+                            i += Integer.parseInt(split[1]) - 1;
+                            break;
+
+                        case "nop":
+
+                            break;
+                    }
+                } else {
+                    looped = true;
+                }
+            }
+
+            if (!looped) {
+                System.out.println(accumulator);
+            }
+        }
+    }
+}
