d0e2b23a2223c0aac62b51496da6eacf53ac53b0
Hax <hax@bbn.one>
2021-11-30 16:31:13 +0100

    Update startall.sh

--- startall.sh
+++ startall.sh
@@ -6,3 +6,7 @@ screen -dmS aoc-server ./startserver.sh
 cd ./clients/java/
 chmod 755 ./start.sh
 screen -dmS aoc-java ./start.sh
+
+cd ./clients/ts/
+chmod 755 ./start.sh
+screen -dmS aoc-ts ./start.sh
