482791e230e433fe2f28cfbcfac9f4cb737266f8
Schlauer-Hax <hax@bbn.family>
2022-12-02 14:27:24 +0100

    Updated Year 2022, Day 2

--- clients/typescript/solutions/S2202.ts
+++ clients/typescript/solutions/S2202.ts
@@ -3,27 +3,12 @@ import ISolution from "./ISolution.ts";
 export default class S2202 implements ISolution {
     name = "S2202";
     firstPart(input: string): number {
-        const wins = {
-            "rock": "scissors",
-            "paper": "rock",
-            "scissors": "paper"
-        } as { [key: string]: string };
-        const playermap = {
-            'X': 'rock',
-            'Y': 'paper',
-            'Z': 'scissors'
-        } as { [key: string]: string };
-        const elvesmap = {
-            'A': 'rock',
-            'B': 'paper',
-            'C': 'scissors'
-        } as { [key: string]: string };
-
+        const wins: { [key: number]: number } = { 0: 2, 1: 0, 2: 1 };
         return input.split("\n").map(line => {
             const [elve, player] = line.split(" ");
-            const playermove = playermap[player];
-            const elvemove = elvesmap[elve];
-            const pluspoints = Object.keys(playermap).indexOf(player) + 1;
+            const playermove = player.charCodeAt(0) - 88;
+            const elvemove = elve.charCodeAt(0) - 65;
+            const pluspoints = playermove + 1;
             if (playermove === elvemove) {
                 return 3 + pluspoints;
             }
@@ -34,41 +19,19 @@ export default class S2202 implements ISolution {
         }).reduce((a, b) => a + b, 0);
     }
     secondPart(input: string): number {
-        const wins = {
-            "rock": "scissors",
-            "paper": "rock",
-            "scissors": "paper"
-        } as { [key: string]: string };
-        const playermap = {
-            'X': 'rock',
-            'Y': 'paper',
-            'Z': 'scissors'
-        } as { [key: string]: string };
-        const elvesmap = {
-            'A': 'rock',
-            'B': 'paper',
-            'C': 'scissors'
-        } as { [key: string]: string };
-
+        const wins: { [key: number]: number } = { 0: 2, 1: 0, 2: 1 };
         return input.split("\n").map(line => {
             const [elve, player] = line.split(" ");
-            const elvemove = elvesmap[elve];
+            const elvemove = elve.charCodeAt(0) - 65;
+            let playermove;
             if (player === 'X') {
-                const playermove = Object.entries(wins).find(([key, value]) => value !== elvemove && key !== elvemove)![0];
-                const pluspoints = Object.values(playermap).indexOf(playermove) + 1;
-                return 0 + pluspoints;
+                playermove = Object.entries(wins).map(e => [Number(e[0]), Number(e[1])]).find(([key, value]) => value !== elvemove && key !== elvemove)![0];
+            } else if (player === 'Y') {
+                playermove = elvemove;
+            } else {
+                playermove = Object.entries(wins).map(e => [Number(e[0]), Number(e[1])]).find(([_, value]) => value === elvemove)![0];
             }
-            if (player === 'Y') {
-                const playermove = elvemove;
-                const pluspoints = Object.values(playermap).indexOf(playermove) + 1;
-                return 3 + pluspoints;
-            }
-            if (player === 'Z') {
-                const playermove = Object.entries(wins).find(([key, value]) => value === elvemove)![0];
-                const pluspoints = Object.values(playermap).indexOf(playermove) + 1;
-                return 6 + pluspoints;
-            }
-            return 0;
+            return playermove + 1 + (player.charCodeAt(0) - 88) * 3;
         }).reduce((a, b) => a + b, 0);
     }
 }
