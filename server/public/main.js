d06e355e0dfa478c84937b8eae98485ef6e19c96
Schlauer-Hax <hax@bbn.family>
2022-12-04 18:37:57 +0100

    Added auto reconnect on website

--- server/public/main.js
+++ server/public/main.js
@@ -1,44 +1,55 @@
-const ws = new WebSocket(document.location.origin.replace('https', 'wss').replace('http', 'ws')+'/wss');
-ws.onopen = () => {
-    document.getElementById('status').innerHTML = 'Connected';
-    document.getElementById('solutions').disabled = false;
-    document.getElementById('input').disabled = false;
-    document.getElementById('run').disabled = false;
-    document.getElementById('solutions').addEventListener('change', () => {
-        ws.send(JSON.stringify({
-            type: 'data',
-            name: document.getElementById('solutions').value,
-        }));
-    });
-    document.getElementById('run').addEventListener('click', () => {
-        document.getElementById('result').innerHTML = 'Waiting for Solutions...';
-        ws.send(JSON.stringify({
-            type: 'run',
-            solution: document.getElementById('solutions').value,
-            input: document.getElementById('input').value,
-        }));
-    });
-};
+function startConnection() {
+    const ws = new WebSocket(document.location.origin.replace('https', 'wss').replace('http', 'ws') + '/wss');
 
-ws.onmessage = (event) => {
-    const json = JSON.parse(event.data)
-    if (json.type === 'solutions') {
-        document.getElementById('solutions').innerHTML =
-            json.data
-                .sort((a, b) => b.replace('S', '') - a.replace('S', ''))
-                .map((solution) => `<option>${solution}</option>`)
-                .join('');
-        ws.send(JSON.stringify({
-            type: 'data',
-            name: document.getElementById('solutions').value
-        }));
-    } else if (json.type === 'data') {
-        document.getElementById('input').value = json.data;
-    } else if (json.type === 'result') {
-        document.getElementById('result').innerHTML = `${json.data} - ${json.time}ms`;
+    ws.onopen = () => {
+        document.getElementById('solutions').addEventListener('change', () => {
+            ws.send(JSON.stringify({
+                type: 'data',
+                name: document.getElementById('solutions').value,
+            }));
+        });
+        document.getElementById('run').addEventListener('click', () => {
+            document.getElementById('result').innerHTML = 'Waiting for Solutions...';
+            ws.send(JSON.stringify({
+                type: 'run',
+                solution: document.getElementById('solutions').value,
+                input: document.getElementById('input').value,
+            }));
+        });
+        updateStatus(ws);
+    };
+
+    ws.onmessage = (event) => {
+        const json = JSON.parse(event.data)
+        if (json.type === 'solutions') {
+            document.getElementById('solutions').innerHTML =
+                json.data
+                    .sort((a, b) => b.replace('S', '') - a.replace('S', ''))
+                    .map((solution) => `<option>${solution}</option>`)
+                    .join('');
+            ws.send(JSON.stringify({
+                type: 'data',
+                name: document.getElementById('solutions').value
+            }));
+        } else if (json.type === 'data') {
+            document.getElementById('input').value = json.data;
+        } else if (json.type === 'result') {
+            document.getElementById('result').innerHTML = `${json.data} - ${json.time}ms`;
+        }
+    }
+
+    ws.onclose = () => {
+        updateStatus(ws);
+        setTimeout(() => startConnection(), 1000);
+    }
+
+    function updateStatus(ws) {
+        const connected = ws.readyState === 1;
+        document.getElementById('status').innerHTML = connected ? 'Connected' : 'Disconnected, trying to reconnect...';
+        document.getElementById('solutions').disabled = !connected;
+        document.getElementById('input').disabled = !connected;
+        document.getElementById('run').disabled = !connected;
     }
 }
 
-ws.onclose = () => {
-    console.log('Disconnected from server');
-}
+startConnection();
