b2a1c949a998b2ba21246abe8a700a1e3f1aed12
Schlauer-Hax <hax@bbn.family>
2023-11-29 21:19:37 +0100

    small fixes

--- server/apiserver.ts
+++ server/apiserver.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std/http/mod.ts";
+import { serve } from "https://deno.land/std@0.190.0/http/mod.ts";
 import WebServer from "./webserver.ts";
 
 export default class ApiServer {
