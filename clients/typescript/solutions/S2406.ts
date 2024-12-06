727ef33b4436bb7d536eabbe4a261e45c78ea1eb
Schlauer-Hax <hax@bbn.family>
2024-12-06 08:56:55 +0100

    Year 2024, Day 6

--- clients/typescript/solutions/S2406.ts
+++ clients/typescript/solutions/S2406.ts
@@ -1,5 +1,4 @@
 import ISolution from "./ISolution.ts";
-import routine from "https://deno.land/x/routine/mod.ts";
 
 export default class S2406 implements ISolution {
     firstPart(input: string): (number | string) | Promise<number | string> {
@@ -14,7 +13,7 @@ export default class S2406 implements ISolution {
                 }
             }
         }
-        const poss: {x: number, y: number}[] = [];
+        const poss: { x: number, y: number }[] = [];
         poss.push({ x: currentPos.x, y: currentPos.y });
         walk: while (true) {
             switch (direction) {
@@ -69,11 +68,10 @@ export default class S2406 implements ISolution {
         }
         return poss.filter((pos, index) => poss.findIndex(p => p.x === pos.x && p.y === pos.y) === index).length;
     }
-    async secondPart(input: string): Promise<string | number> {
+    secondPart(input: string) {
         const coords = input.split("\n").map(line => line.split(""));
-        function tryWith(coords: string[][]): number {
-            const poss: {x: number, y: number}[] = [];
-            const turn: {x: number, y: number, t: string}[] = [];
+        function tryWith(coords: string[][]): [ number, { x: number, y: number }[] ] {
+            const turn: { x: number, y: number, t: string }[] = [];
             let direction = "up";
             const currentPos = { x: 0, y: 0 };
             for (const y in coords) {
@@ -84,10 +82,11 @@ export default class S2406 implements ISolution {
                     }
                 }
             }
+            const poss: { x: number, y: number }[] = [];
             poss.push({ x: currentPos.x, y: currentPos.y });
             walk: while (true) {
                 if (turn.some((t, i) => turn.findIndex(tt => tt.x === t.x && tt.y === t.y && tt.t === t.t) !== i)) {
-                    return 1;
+                    return [ 1, poss ];
                 }
                 switch (direction) {
                     case "up":
@@ -99,6 +98,7 @@ export default class S2406 implements ISolution {
                             turn.push({ x: currentPos.x, y: currentPos.y, t: "right" });
                         } else {
                             currentPos.y--;
+                            poss.push({ x: currentPos.x, y: currentPos.y });
                         }
                         break;
                     case "right":
@@ -110,6 +110,7 @@ export default class S2406 implements ISolution {
                             turn.push({ x: currentPos.x, y: currentPos.y, t: "down" });
                         } else {
                             currentPos.x++;
+                            poss.push({ x: currentPos.x, y: currentPos.y });
                         }
                         break;
                     case "down":
@@ -121,6 +122,7 @@ export default class S2406 implements ISolution {
                             turn.push({ x: currentPos.x, y: currentPos.y, t: "left" });
                         } else {
                             currentPos.y++;
+                            poss.push({ x: currentPos.x, y: currentPos.y });
                         }
                         break;
                     case "left":
@@ -132,23 +134,23 @@ export default class S2406 implements ISolution {
                             turn.push({ x: currentPos.x, y: currentPos.y, t: "up" });
                         } else {
                             currentPos.x--;
+                            poss.push({ x: currentPos.x, y: currentPos.y });
                         }
                         break;
 
                 }
             }
-            return 0;
+            return [ 0, poss.filter((pos, index) => poss.findIndex(p => p.x === pos.x && p.y === pos.y) === index) ];
         }
+        const [ _, fields ] = tryWith(coords);
         const routines = [];
-        for (const y in coords) {
-            for (const x in coords[ y ]) {
-                if (coords[ y ][ x ] === ".") {
-                    coords[ y ][ x ] = "#";
-                    routines.push(routine(tryWith, coords));
-                    coords[ y ][ x ] = ".";
-                }
+        for (const { x, y } of fields) {
+            if (coords[ y ][ x ] === ".") {
+                coords[ y ][ x ] = "#";
+                routines.push(tryWith(coords.copyWithin(0, 0)));
+                coords[ y ][ x ] = ".";
             }
         }
-        return await Promise.all(routines).then(res => res.reduce((a, b) => a + b, 0));
+        return routines.reduce((a, b) => a + b[ 0 ], 0);
     }
 }
